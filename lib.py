05fd6ce812653f91dbd3b2e6cbd741efb4bd9199
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-10-31 14:35:08 +0530

    .

--- lib.py
+++ lib.py
@@ -2,30 +2,16 @@ from sqlite3 import connect
 from data import tables
 
 def select(table_name):
-    keys = tables[table_name]
-    with connect('database.db') as con:
-        cur = con.cursor()
-        cmd = f"SELECT * FROM {table_name}"
-        cur.execute(cmd)
-        data = cur.fetchall()
-        return data 
+    return run_query(f"SELECT * FROM {table_name}")
 
 def insert(table_name, dict):
     keys = tables[table_name]['keys']
     values = [dict[key] for key in keys]
         
-    with connect('database.db') as con: 
-        cur = con.cursor()
-        cmd = f"INSERT INTO {table_name} VALUES('" + "','".join(values) + "');"
-        print(cmd)
-        cur.execute(cmd)
-        con.commit()
+    run_query(f"INSERT INTO {table_name} VALUES('" + "','".join(values) + "');")
     
 def delete(table_name):
-    with connect('database.db') as con:
-        cur = con.cursor()
-        cmd = f"DELETE FROM {table_name};"
-        cur.execute(cmd)
+    run_query(f"DELETE FROM {table_name};")
         
 def run_query(cmd):
     with connect('database.db') as con:
